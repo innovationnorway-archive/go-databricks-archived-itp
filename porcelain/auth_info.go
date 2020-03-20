e30f3c7b1d34b029b86e4d20064394aa93d48f66
Joakim Bakke Hellum <joakim.bakke.hellum@innovasjonnorge.no>
2020-03-20 20:15:07 +0100

    Add `AzureDatabricksApplicationID` to porcelain

--- porcelain/auth_info.go
+++ porcelain/auth_info.go
@@ -5,6 +5,10 @@ import (
 	"github.com/go-openapi/strfmt"
 )
 
+const (
+	AzureDatabricksApplicationID = "2ff814a6-3304-4ab8-85cb-cd0e6f879c1d"
+)
+
 // AzureAuth provides Azure Databricks auth info writer
 func AzureAuth(token, managementToken, workspaceID string) runtime.ClientAuthInfoWriter {
 	return runtime.ClientAuthInfoWriterFunc(func(r runtime.ClientRequest, _ strfmt.Registry) error {
