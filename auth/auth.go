06fb3a88045b7884ebe0ec35bfefacc5696fe66e
Joakim Bakke Hellum <joakim.bakke.hellum@innovasjonnorge.no>
2020-03-24 03:15:38 +0100

    Update auth
    Add `NewTokenAuthorizer`
    Update `NewAzureDatabricksAuthorizer`
    Note: This is work in progress

--- auth/auth.go
+++ auth/auth.go
@@ -2,8 +2,6 @@ package auth
 
 import (
 	"fmt"
-	"net/http"
-	"strings"
 
 	"github.com/Azure/go-autorest/autorest"
 )
@@ -19,8 +17,26 @@ const (
 	headerXDatabricksAzureWorkspaceResourceID = "X-Databricks-Azure-Workspace-Resource-Id"
 )
 
-// AzureDatabricksAuthorizer implements an authorization for Databricks Azure AD Authentication
-// this can be used for interaction with Azure Databricks API
+// TokenAuthorizer implements the bearer authorization
+type TokenAuthorizer struct {
+	token string
+}
+
+// NewTokenAuthorizer crates a TokenAuthorizer using the given token provider
+func NewTokenAuthorizer(token string) *TokenAuthorizer {
+	return &TokenAuthorizer{token: token}
+}
+
+// WithAuthorization returns a PrepareDecorator that adds an HTTP Authorization header whose
+// value is "Bearer " followed by the token.
+func (t *TokenAuthorizer) WithAuthorization() autorest.PrepareDecorator {
+	headers := make(map[string]interface{})
+	headers[headerAuthorization] = fmt.Sprintf("Bearer %s", t.token)
+
+	return autorest.NewAPIKeyAuthorizerWithHeaders(headers).WithAuthorization()
+}
+
+// AzureDatabricksAuthorizer implements authorization for Azure Databricks.
 type AzureDatabricksAuthorizer struct {
 	token           string
 	managementToken string
@@ -28,35 +44,22 @@ type AzureDatabricksAuthorizer struct {
 }
 
 // NewAzureDatabricksAuthorizer creates a AzureTokenAuthorizer using the given credentials
-func NewAzureDatabricksAuthorizer(token, managementToken, workspaceID string) (*AzureDatabricksAuthorizer, error) {
-	if strings.TrimSpace(token) == "" {
-		return nil, fmt.Errorf("token cannot be empty")
-	}
-
+func NewAzureDatabricksAuthorizer(token, managementToken, workspaceID string) *AzureDatabricksAuthorizer {
 	return &AzureDatabricksAuthorizer{
 		token:           token,
 		managementToken: managementToken,
 		workspaceID:     workspaceID,
-	}, nil
+	}
 }
 
 // WithAuthorization returns a PrepareDecorator that adds an HTTP Authorization header using the
 // primary token along with the Azure Databricks authorization headers using the management token
 // and workspace ID
-// This can be used for Databricks Azure AD Authentication
 func (ada *AzureDatabricksAuthorizer) WithAuthorization() autorest.PrepareDecorator {
-	return func(p autorest.Preparer) autorest.Preparer {
-		return autorest.PreparerFunc(func(r *http.Request) (*http.Request, error) {
-			r, err := p.Prepare(r)
-			if err != nil {
-				return r, err
-			}
+	headers := make(map[string]interface{})
+	headers[headerAuthorization] = fmt.Sprintf("Bearer %s", ada.token)
+	headers[headerXDatabricksAzureSPManagementToken] = ada.managementToken
+	headers[headerXDatabricksAzureWorkspaceResourceID] = ada.workspaceID
 
-			return autorest.Prepare(r,
-				autorest.WithHeader(headerAuthorization, ada.token),
-				autorest.WithHeader(headerXDatabricksAzureSPManagementToken, ada.managementToken),
-				autorest.WithHeader(headerXDatabricksAzureWorkspaceResourceID, ada.workspaceID),
-			)
-		})
-	}
+	return autorest.NewAPIKeyAuthorizerWithHeaders(headers).WithAuthorization()
 }
